3e9471a6064c25bf160a86bc60beeb6fdc7bcd76
Akilah Hunte <ahunt173@gmail.com>
2020-12-06 16:58:58 -0500

    Fixing HTML issues

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,31 +1,29 @@
-from flask import Flask,render_template,redirect
-from flask_pymongo 
-import PyMongo 
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo 
 import scrape_mars
 
+app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.current_mars_info
-collection = db.mars_info
+mongo = PyMongo(app)
+
 
 #Get the main route and connection code between mongo and the app.py 
 #def scrape(): #upsert #update (comes first) to retrieve information 
-@app.route("/scrape")
-def scrape():
-    mars = mongo.db.mars_info
-    data = scrape_mars.scrape()
-    mars.update(
-       {},
-       data,
-       upsert=True
-    )
-    return redirect("http://localhost:5000/", code=302)
-
+@app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html")
+    return render_template("index.html", mars = mars)
 
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update(
+       {},
+       mars_data,
+       upsert=True
+    )
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True),
